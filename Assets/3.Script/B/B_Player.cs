702ba591a7d4f65f56bcb3710ba26b91fab4f1a4
J5-67 <gjong567@gmail.com>
2025-12-08 10:30:18 +0900

    [B] Main

--- Assets/3.Script/B/B_Player.cs
+++ Assets/3.Script/B/B_Player.cs
@@ -7,16 +7,16 @@ public class B_Player : MonoBehaviour
     [SerializeField] private float invincibleTime = 2f;
     [SerializeField] private float blinkSpeed = 0.1f;
     [SerializeField] private int maxHealth = 3;
-    private MeshRenderer playerMR;
+    //private MeshRenderer playerMR;
     private Color originalColor;
     private bool isInvincible = false;
     private int currentHealth;
 
     private void Awake()
     {
-        TryGetComponent(out playerMR);
+        //TryGetComponent(out playerMR);
 
-        originalColor = playerMR.material.color;
+        //originalColor = playerMR.material.color;
     }
 
     void Start()
@@ -40,30 +40,30 @@ public class B_Player : MonoBehaviour
         }
         else
         {
-            StartCoroutine(Invincible_co());
+            //StartCoroutine(Invincible_co());
         }
     }
 
-    private IEnumerator Invincible_co()
-    {
-        isInvincible = true;
-
-        float timer = 0f;
-
-        while (timer < invincibleTime)
-        {
-            if (playerMR != null) playerMR.material.color = Color.red;
-            yield return new WaitForSeconds(blinkSpeed);
-
-            if (playerMR != null) playerMR.material.color = originalColor;
-            yield return new WaitForSeconds(blinkSpeed);
-
-            timer += (blinkSpeed * 2);
-        }
-
-        if (playerMR != null) playerMR.material.color = originalColor;
-        isInvincible = false;
-    }
+    //private IEnumerator Invincible_co()
+    //{
+    //    isInvincible = true;
+    //
+    //    float timer = 0f;
+    //
+    //    while (timer < invincibleTime)
+    //    {
+    //        if (playerMR != null) playerMR.material.color = Color.red;
+    //        yield return new WaitForSeconds(blinkSpeed);
+    //
+    //        if (playerMR != null) playerMR.material.color = originalColor;
+    //        yield return new WaitForSeconds(blinkSpeed);
+    //
+    //        timer += (blinkSpeed * 2);
+    //    }
+    //
+    //    if (playerMR != null) playerMR.material.color = originalColor;
+    //    isInvincible = false;
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
