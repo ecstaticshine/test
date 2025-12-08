cf3f4a0960d9f75bb6cc62105b1dd1f7e4adddb5
J5-67 <gjong567@gmail.com>
2025-12-08 10:59:29 +0900

    [B] 스크립트 수정

--- Assets/3.Script/B/B_Move.cs
+++ Assets/3.Script/B/B_Move.cs
@@ -7,8 +7,10 @@ using UnityEngine.InputSystem;
 public class B_Move : MonoBehaviour
 {
     [SerializeField] private Transform[] lanes;
+    [SerializeField] private float gravityScale = 2.0f;
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpPower = 5f;
+    private Animator playerAnimator;
     private Rigidbody playerRigid;
     private int currentLane = 1;
     private bool isJumping = false;
@@ -16,6 +18,9 @@ public class B_Move : MonoBehaviour
     private void Awake()
     {
         TryGetComponent(out playerRigid);
+        TryGetComponent(out playerAnimator);
+
+        playerRigid.useGravity = false;
     }
 
     private void Update()
@@ -25,6 +30,11 @@ public class B_Move : MonoBehaviour
         transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 
+    private void FixedUpdate()
+    {
+        playerRigid.AddForce(Physics.gravity * gravityScale, ForceMode.Acceleration);
+    }
+
     public void OnMove(InputValue value)
     {
         float input = value.Get<float>();
@@ -32,10 +42,12 @@ public class B_Move : MonoBehaviour
         if (input > 0)
         {
             MoveLane(1);
+            StartCoroutine(MoveAnimation_co("MoveRight"));
         }
         else if (input < 0)
         {
             MoveLane(-1);
+            StartCoroutine(MoveAnimation_co("MoveLeft"));
         }
     }
 
@@ -43,6 +55,8 @@ public class B_Move : MonoBehaviour
     {
         if (value.isPressed && !isJumping)
         {
+            playerAnimator.SetBool("IsJump", true);
+
             isJumping = true;
 
             playerRigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
@@ -65,6 +79,17 @@ public class B_Move : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        playerAnimator.SetBool("IsJump", false);
+
         isJumping = false;
+    }
+
+    private IEnumerator MoveAnimation_co(string para_name)
+    {
+        playerAnimator.SetBool(para_name, true);
+
+        yield return new WaitForSeconds(0.05f);
+
+        playerAnimator.SetBool(para_name, false);
     }
 }
